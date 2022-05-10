9208ef1f7935431ea5510e593649504daa93ffe4
Danilo Santos <danilo99ambrosio@hotmail.com>
2022-05-10 00:06:18 -0300

    primeiro model-view-controller-department

---
+++ ProjetoFinal/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoFinal.Models;
+
+namespace ProjetoFinal.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { ID = 1, Name = "Eletrônicos" });
+            list.Add(new Department { ID = 2, Name = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
---
+++ ProjetoFinal/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoFinal.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+
+
+
+    }
+}
